17c1c6cc8a0d500d2b7cee053424ce9af6b4d213
Adrian Kwok <adrianandrobin12@yahoo.com>
2018-04-09 01:01:40 +0100

    solved ch1_8 but as always, solutions are mostly brute force and not elegant :(

--- Ch1/ch1_8.cpp
+++ Ch1/ch1_8.cpp
@@ -6,19 +6,40 @@
  */
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main(){
     int m = 3;
     int n = 4;
-    int matrix[m][n];
+    int matrix[m][n] = {{1,5,3,0},{2,8,2,1},{0,2,4,6}};
+    vector<int> pairs;
     for (int i = 0; i < m; i++){
         for(int j = 0; j < n; j++){
             if(matrix[i][j] == 0){
-                //store i and j in an array
+                pairs.push_back(i);
+                pairs.push_back(j);
             }
         }
     }
-    //loop for replacing column by using indeces stored in array for i and st 
+    for(int i = 0; i < pairs.size(); i++){
+        cout << pairs[i] << ", " << pairs[i+1] << endl;
+        i++;
+    }
+    for(int i = 0; i < pairs.size(); i++){
+        for(int j = 0; j < n; j++){
+            matrix[pairs[i]][j] = 0;
+        }
+        i++;
+        for(int j = 0; j < m; j++){
+            matrix[j][pairs[i]] = 0;
+        }
+    }
+    for (int i = 0; i < m; i++){
+        for(int j = 0; j < n; j++){
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
 }
