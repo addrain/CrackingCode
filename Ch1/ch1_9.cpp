e81176852d01326b7d6c49f5111c5c9ff6fadac6
Adrian Kwok <adrianandrobin12@yahoo.com>
2018-04-10 20:25:40 -0700

    added ch1_9.cpp file, empty

---
+++ Ch1/ch1_9.cpp
@@ -0,0 +1,16 @@
+/**
+ * Adrian Kwok
+ * 
+ * String Rotation: Assume you have a method isSubstring which 
+ * checks if one word is a substring of another. Given two strings, 
+ * s1 and s2, write code to check if s2 is a rotation of s1 using 
+ * only one call to isSubstring (e.g., "waterbottle" is a rotation 
+ * of "erbottlewat").
+ * 
+ * Will work on later.
+ * Can double one of the strings and call isSubstring.  Ex: waterbottle is a substring of erbottlewaterbottlewat
+ * 
+ * or, get the first unique letter or pair of letters to rotate the other string.
+ * 
+ * I like the first solution better.
+ * /
